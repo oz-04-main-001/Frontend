608e1dc42d9a8bc1cb356c04ef74ed78c4f8f35e
kiseon77 <kieson.han77@gmail.com>
2024-10-16 12:55:24 +0900

    DESIGN: #64 ui버튼생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Search from './pages/search';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
