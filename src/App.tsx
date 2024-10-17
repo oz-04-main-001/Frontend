39e1d18dd0c845517314fb39523999d71a6545aa
dnwjd0218 <dnwjd1325@naver.com>
2024-10-17 16:48:44 +0900

    DESIGN:dropdown수정, logo, counter, icons완성

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Search from './pages/search';
 import Accommodations from './pages/room/Accommodations';
-import Popup from './components/Popup';
+import Dropdown from './assets/Dropdown';
+import Logo from './assets/logo'
+import Counter from './assets/Counter';
 
 function App() {
   return (
     <BrowserRouter>
-      <Popup />
+    <Logo width='60px' height='60px' color='#778cd2'/>
+    <Counter size={10}/>
+      <Dropdown menuItems={['yejin','Kim yejin','puhaha','ddong','hahahahah']} width='250px' />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
