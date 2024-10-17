63896810b65e8c8926389ca170de92bb4ebe7364
dnwjd0218 <dnwjd1325@naver.com>
2024-10-17 18:21:09 +0900

    DESIGN:헤더 완성/icons 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Search from './pages/search';
 import Accommodations from './pages/room/Accommodations';
-
-
+import Header from './assets/Header';
 function App() {
   return (
     <BrowserRouter>
+    <Header labels={['게스트메인','새 숙소 등록','로그아웃']}/>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
