ac7eda6f7302bbb2b0bbd7910a1643ba26cadede
kiseon77 <kieson.han77@gmail.com>
2024-10-17 16:50:28 +0900

    SYTLE:숙소 카드 제작

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Search from './pages/search';
 import Accommodations from './pages/room/Accommodations';
-import Popup from './components/Popup';
 
 function App() {
   return (
     <BrowserRouter>
-      <Popup />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
