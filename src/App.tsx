91e7e5d1c03fc106d084b3659495fa3dd0b76ad1
kiseon77 <kieson.han77@gmail.com>
2024-10-19 00:54:29 +0900

    DESIGN : 호스트 예약관리

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,23 @@ import Search from './pages/search';
 import Accommodations from './pages/room/Accommodations';
 import Stateroom from './pages/room/Stateroom';
 import Orders from './pages/room/Orders';
-import SelectType from './pages/host/setAccommodations/SelectType';
-import Chips from './assets/Chips';
-
+import Management from './pages/host/Management';
 
 function App() {
   return (
     <BrowserRouter>
-    <SelectType/>
-    <Chips text='테스트 할거라고 !!!!'/>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/search" element={<Search />} />
         <Route path="/accommodations" element={<Accommodations />} />
         <Route path="/stateroom" element={<Stateroom />} />
         <Route path="/stateroom/order" element={<Orders />} />
+        <Route path="/host" element={<Management />}>
+          <Route path="" />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-
