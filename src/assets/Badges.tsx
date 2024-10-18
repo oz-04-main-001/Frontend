9faacabfc42282a2e9519206241ffe52560bcfdf
sangmin0310-afk <minism0310@naver.com>
2024-10-18 18:25:00 +0900

    DESIGN/ 마이페이지 카드#98

--- src/assets/Badges.tsx
+++ src/assets/Badges.tsx
@@ -1,14 +1,14 @@
-// 상만
 interface BadgesProps {
   label: string;
   status: '예정' | '이용중' | '완료' | '취소';
 }
 
+// Badges 컴포넌트 정의
 export function Badges({ label, status }: BadgesProps) {
   const badgeClasses: { [key: string]: string } = {
-    예정: 'w-[70px] h-[26px] bg-primary-pailBlue300',
+    예정: 'w-[70px] h-[26px] bg-primary-300',
     이용중: 'w-[57px] h-[26px] bg-state-warn',
-    완료: 'w-[70px] h-[26px] bg-basic-300',
+    완료: 'w-[70px] h-[26px] bg-gray-300',
     취소: 'w-[57px] h-[26px] bg-state-err',
   };
 
