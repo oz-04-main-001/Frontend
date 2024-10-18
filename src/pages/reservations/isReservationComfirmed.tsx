d8cacbf5acb45987f7bb7aff1560f4c6c78525c0
Soom <doda2031@naver.com>
2024-10-19 04:38:10 +0900

    DESIGN: 게스트 예약대기/확정 페이지 완료

--- src/pages/reservations/isReservationComfirmed.tsx
+++ src/pages/reservations/isReservationComfirmed.tsx
@@ -6,17 +6,17 @@ import RefundNotice from './RefundNotice';
 export default function IsReservationConfirmed() {
   return (
     <>
-      <div className="flex flex-col bg-gray-50 w-svw h-auto">
-        <div className="pt-24 pl-44 pb-8 bg-white">
-          <AccmoInfoCard state="예정" />
+      <div className="flex flex-col bg-gray-50 w-svw max-h-fit">
+        <div className=" pt-24 pl-48 pb-8 bg-white">
+          <AccmoInfoCard />
         </div>
-        <div className="mt-3 pl-44 pt-8 pb-14 bg-white">
+        <div className="mt-3 pl-48 pt-8 pb-14 bg-white">
           <GuestInfo />
         </div>
-        <div className="mt-3 pl-44 pt-8 pb-14 w-screen bg-white">
+        <div className="mt-3 pl-48 pt-8 pb-14 w-screen bg-white">
           <RefundNotice />
         </div>
-        <div className="mt-3 pl-44 pt-8 pb-14 flex flex-row w-screen bg-white">
+        <div className="mt-3 pl-48 pt-8 pb-14 flex flex-row w-screen bg-white">
           <PaymentInfo />
         </div>
         <div className="flex justify-between pl-44 mt-3 text-gray-500 p-4 bg-white">
