b5f237da2df5742c38b467065e2642325814bcc9
dnwjd0218 <dnwjd1325@naver.com>
2024-10-18 23:13:29 +0900

    DESIGN:건물타입

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import Accommodations from './pages/room/Accommodations';
 import Stateroom from './pages/room/Stateroom';
 import Orders from './pages/room/Orders';
 import SelectType from './pages/host/setAccommodations/SelectType';
-import Chips from './assets/Chips';
+import StructureType from './pages/host/setAccommodations/StructureType';
+import MultiAccommodations from './pages/host/setAccommodations/MultiAccommodations';
+import OnlyStaterRoom from './pages/host/setAccommodations/OnlyStaterRoom';
 
 
 function App() {
   return (
     <BrowserRouter>
-    <SelectType/>
-    <Chips text='테스트 할거라고 !!!!'/>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<SelectType />} />
+        <Route path='/structure-type' element={<StructureType/>} />
+        <Route path='/only-starter-room'element={<OnlyStaterRoom/>} />
+        <Route path='/multi-accommodations' element={<MultiAccommodations/>}/>
         <Route path="/search" element={<Search />} />
         <Route path="/accommodations" element={<Accommodations />} />
         <Route path="/stateroom" element={<Stateroom />} />
