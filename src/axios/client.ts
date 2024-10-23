26d9e35501b8242ceeb7b45db88dd55f10a02b79
sangmin0310-afk <minism0310@naver.com>
2024-10-23 18:18:23 +0900

    feature/#139

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -4,13 +4,13 @@ const client = axios.create({
   baseURL: serverURL,
 });
 
-// client.interceptors.request.use(function (config) {
-//   const token = 1234;
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
+client.interceptors.request.use(function (config) {
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5NjYyMDc3LCJpYXQiOjE3Mjk2NjAyNzcsImp0aSI6ImEzMTZlODU0MjUyNjQ1OTQ4OTBhYjdmMTc4ZDA1NjMwIiwidXNlcl9pZCI6MX0.-vCzEFObt3CirlhmeDgWFSkejLb0h3-Lw83Y7PhPf94";
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-//   return config;
-// });
+  return config;
+});
 
 export default client;
