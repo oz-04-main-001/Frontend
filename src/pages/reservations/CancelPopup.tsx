4a13808ad87d57f5450367a5b2338668486491dc
kiseon77 <kieson.han77@gmail.com>
2024-11-01 15:03:35 +0900

    FEAT : 예약취소 정보

--- src/pages/reservations/CancelPopup.tsx
+++ src/pages/reservations/CancelPopup.tsx
@@ -1,14 +1,28 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { putBookingCancel } from '../../axios/orderApi';
 import Popup from '../../components/Popup';
 import RefundNotice from './RefundNotice';
 import ReservAccoCard from './ReservAccoCard';
-
-const CancelPopup: React.FC = () => {
+import { OrderInfo } from './ReservationCompleted';
+import usePopupStore from '../../stores/usePopupStore';
+interface CancelPopupProps {
+  orderInfo: OrderInfo | null;
+}
+const CancelPopup: React.FC<CancelPopupProps> = ({ orderInfo }) => {
   const { orderId } = useParams();
+  const navigate = useNavigate();
+
+  const closePopup = () => {
+    usePopupStore.getState().closePopup();
+  };
   const fetchGetLoad = async () => {
     if (orderId) {
       await putBookingCancel(orderId);
+      navigate(`/reservation/stateroom/order/info/${orderId}`, {
+        replace: true,
+      });
+      closePopup();
+      window.location.reload();
     } else {
       console.error('Order ID is undefined');
     }
@@ -23,11 +37,18 @@ const CancelPopup: React.FC = () => {
       }}
     >
       <div className="flex flex-col text-left">
-        <ReservAccoCard />
+        <ReservAccoCard
+          accommodation={orderInfo?.accommodation_info.name}
+          room={orderInfo?.room_info.name}
+          guestsCount={orderInfo?.guests_count}
+          bedType={null}
+          bedCount={orderInfo?.room_info.bed_info.total_beds}
+          roomCount={orderInfo?.room_info.room_count}
+        />
         <div className="text-base text-">
-          <span className="text-xs text-gray-800">체크인</span> 2024.12.02 15:00{' '}
-          ~ <span className="text-xs text-gray-800">체크아웃 </span> 2024.12.03
-          11:00
+          <span className="text-xs text-gray-800">
+            {orderInfo?.check_in_datetime} ~ {orderInfo?.check_out_datetime}
+          </span>
         </div>
         <div>
           <RefundNotice />
@@ -38,12 +59,16 @@ const CancelPopup: React.FC = () => {
           </p>
           <p className="flex justify-between">
             <span className="inline-block mt-1 text-medium">결제 금액</span>{' '}
-            <span className="inline-block text-xl">34,000원</span>
+            <span className="inline-block text-xl">
+              {orderInfo?.total_price}원
+            </span>
           </p>
           <hr className="mt-3" />
           <p className="flex justify-between my-5 text-2xl text-state-err">
             <span className="inline-block text-xl">환불 예정 금액</span>
-            <span className="inline-block text-black">34,000원</span>
+            <span className="inline-block text-black">
+              {orderInfo?.total_price}원
+            </span>
           </p>
         </div>
       </div>
