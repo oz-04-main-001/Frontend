67f41b771de6a7e837a9d68cc267cea51a3302f0
Soom <doda2031@naver.com>
2024-11-04 18:34:53 +0900

    CHOR: consolelog 주석 처리

--- src/pages/host/index.tsx
+++ src/pages/host/index.tsx
@@ -1,6 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header, { labels } from '../../assets/Header';
 import Layout from '../../layouts/Layout2';
+<<<<<<< HEAD
+=======
+import useAuthStore from '../../stores/useAuthStore';
+
+>>>>>>> 342ebf0 (CHOR: consolelog 주석 처리)
 export default function index() {
   const headerLabelArr: labels[] = [
     { title: '게스트 메인', link: '/' },
