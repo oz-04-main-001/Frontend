d9a45edda35df1da78ba59ab3ccf981d92c7b60f
kiseon77 <kieson.han77@gmail.com>
2024-11-06 01:47:30 +0900

    FIX : 라우트 수정

--- src/axios/host.tsx
+++ src/axios/host.tsx
@@ -2,13 +2,6 @@ import axios from 'axios';
 const serverURL = import.meta.env.VITE_SERVER_URL;
 
 export const postHostCreate = async (formData: FormData): Promise<any> => {
-  for (let key of formData.keys()) {
-    console.log('key', key);
-  }
-  for (let value of formData.values()) {
-    console.log('value', value);
-  }
-
   const token = localStorage.getItem('auth_token');
 
   if (token) {
